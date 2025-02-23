21f997ceb2aa1ac372086d299feae9d6eb3d9b33
Tyagi <sutyagi@galaxe.com>
2025-02-23 11:06:35 -0500

    Added REST service for derug details

---
+++ src/main/java/com/test/drugapi/model/Drug.java
@@ -0,0 +1,23 @@
+package com.test.drugapi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Drug {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    // Getters and Setters
+}
